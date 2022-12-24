995b65737782b5fab6e154b5356424f25f2a6a3d
Adarsh Kumar <adarsh.kumar@sourcefuse.com>
2022-12-24 14:41:23 +0530

    added heapify function in heap section

--- heap/insertion.cpp
+++ heap/insertion.cpp
@@ -53,6 +53,22 @@ class heap {
         }
 
     }
+    void heapify(int arr[], int n, int i) {
+        int largest = i;
+        int left = 2*i;
+        int right = 2*i+1;
+
+        if(left <= n && arr[largest] < arr[left]) {
+            largest = left;
+        }
+        if(right <= n && arr[largest] < arr[right]) {
+            largest = right;
+        }
+        if(largest != i) {
+            swap(arr[largest], arr[i]);
+            heapify(arr, n, largest);
+        }
+    }
 
     void print() {
         for(int i=1;i<=size;i++) {
